0b4a5586042a6ae922c6f580982a63c74221dbbf
jehanxaibahmed <jehanxaibahmed@gmail.com>
2021-06-24 22:10:59 +0500

    Changes

--- static/js/xlsx.0057cc49.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpinvoicemate=this.webpackJsonpinvoicemate||[]).push([[3],{1840:function(n,i){},1909:function(n,i){},1910:function(n,i){}}]);
-//# sourceMappingURL=xlsx.0057cc49.chunk.js.map
