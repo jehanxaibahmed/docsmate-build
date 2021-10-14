b154852da43ee2855bfd37a5fceaabc19d8e57de
jehanxaibahmed <jehanxaibahmed@gmail.com>
2021-10-14 17:23:08 +0500

    Changes

---
+++ precache-manifest.edfe1343b5a373c2d2ceb782f3522181.js
@@ -0,0 +1,210 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0dea388707cbcbe41fe09a0f57cf043",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4b4ea96943057ae6827",
+    "url": "/static/css/4.d3d5f4db.chunk.css"
+  },
+  {
+    "revision": "5e301868730c701d0d9e",
+    "url": "/static/css/main.645195fe.chunk.css"
+  },
+  {
+    "revision": "fe1d068ecb1da1e13851",
+    "url": "/static/js/0.22a4a663.chunk.js"
+  },
+  {
+    "revision": "ea6be57aaf29d7024eb3",
+    "url": "/static/js/10.5f75863e.chunk.js"
+  },
+  {
+    "revision": "54a13f759b1799645c0f4c440498a75c",
+    "url": "/static/js/10.5f75863e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4b4ea96943057ae6827",
+    "url": "/static/js/4.9fffe26c.chunk.js"
+  },
+  {
+    "revision": "9c6a170186faec22213d8a4294c01606",
+    "url": "/static/js/4.9fffe26c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c56e10c42ad4983e4b29",
+    "url": "/static/js/5.6b31824c.chunk.js"
+  },
+  {
+    "revision": "4d9bba4fd11162c82e77",
+    "url": "/static/js/6.65b894b9.chunk.js"
+  },
+  {
+    "revision": "783f14fa45b10e088e68f98251448010",
+    "url": "/static/js/6.65b894b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd316294c487ff9b8bf6",
+    "url": "/static/js/7.4bfcebab.chunk.js"
+  },
+  {
+    "revision": "536950dbfc8788735deaf56f21469aea",
+    "url": "/static/js/7.4bfcebab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8557823a7ef59af14c91",
+    "url": "/static/js/8.3a479eb7.chunk.js"
+  },
+  {
+    "revision": "49c2feb24fa8a0f7d99deba74f4e90d7",
+    "url": "/static/js/8.3a479eb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cb0741dc9d1fe5235f1",
+    "url": "/static/js/9.73b5cb7d.chunk.js"
+  },
+  {
+    "revision": "aa0d01a96b90795adfcac80081b04497",
+    "url": "/static/js/9.73b5cb7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e301868730c701d0d9e",
+    "url": "/static/js/main.27210d6c.chunk.js"
+  },
+  {
+    "revision": "68915d09cd132c74bf3e",
+    "url": "/static/js/runtime-main.0af6b9d3.js"
+  },
+  {
+    "revision": "ff6f09b4dd105ed3916d",
+    "url": "/static/js/xlsx.e273c650.chunk.js"
+  },
+  {
+    "revision": "1c0371fbb26d280ef50c989f475cc468",
+    "url": "/static/media/NoStatus.1c0371fb.png"
+  },
+  {
+    "revision": "bc14bfce1341bda02413a929695c3daf",
+    "url": "/static/media/Pending.bc14bfce.png"
+  },
+  {
+    "revision": "de4701a3a7c94dc0b19283117fc053ac",
+    "url": "/static/media/Rejected.de4701a3.png"
+  },
+  {
+    "revision": "0ec678921cbb58c388a9ba77d56d5693",
+    "url": "/static/media/Success.0ec67892.png"
+  },
+  {
+    "revision": "54e9ec5365eeb967838ffd2a35eda50b",
+    "url": "/static/media/avatar-2.54e9ec53.png"
+  },
+  {
+    "revision": "7a6bc10028a79f78e424a9c71499fa77",
+    "url": "/static/media/avatar.7a6bc100.jpg"
+  },
+  {
+    "revision": "6ee66b2cb8a3d9988e37b83b3b4a590e",
+    "url": "/static/media/card-1.6ee66b2c.jpeg"
+  },
+  {
+    "revision": "ae10829f019b0103525a6cd1102ee213",
+    "url": "/static/media/card-2.ae10829f.jpeg"
+  },
+  {
+    "revision": "4ae4eeff542ab32204e8409d170e14a0",
+    "url": "/static/media/card-3.4ae4eeff.jpeg"
+  },
+  {
+    "revision": "3122abf4a3e1067926c08dee7684522d",
+    "url": "/static/media/card-profile1-square.3122abf4.jpg"
+  },
+  {
+    "revision": "5b7c1cad66a2efdbf3604615cc9d7d1c",
+    "url": "/static/media/clint-mckoy.5b7c1cad.jpg"
+  },
+  {
+    "revision": "ebe9884b2152a08a525fcd5054019532",
+    "url": "/static/media/image_placeholder.ebe9884b.jpg"
+  },
+  {
+    "revision": "35f7d126c07a40a893f3f6cc8d1c3717",
+    "url": "/static/media/lock.35f7d126.jpeg"
+  },
+  {
+    "revision": "99c4285722637ebf01e08c01ad60cc77",
+    "url": "/static/media/login.99c42857.jpeg"
+  },
+  {
+    "revision": "eec7c7f60134e712ef7174c96ca7ee5a",
+    "url": "/static/media/logo-white.eec7c7f6.svg"
+  },
+  {
+    "revision": "11056b2f7ed21da72bb6f62371758be6",
+    "url": "/static/media/logo.11056b2f.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d1c4f648504f246ab78751da662c9e4d",
+    "url": "/static/media/logo_color.d1c4f648.png"
+  },
+  {
+    "revision": "aba54d652e5df3b9f1f8024cbeb6630f",
+    "url": "/static/media/marc.aba54d65.jpg"
+  },
+  {
+    "revision": "b8108eb37249cf7a9d4e4672b141d9f5",
+    "url": "/static/media/mastercard.b8108eb3.png"
+  },
+  {
+    "revision": "415388acae2b00ce872ada11d700c18c",
+    "url": "/static/media/org1.415388ac.jpg"
+  },
+  {
+    "revision": "655c5ebc4175c0ca7f7ec6c4d0918278",
+    "url": "/static/media/org2.655c5ebc.jpg"
+  },
+  {
+    "revision": "1106b9c59d5bb53fa1e1d5d5507d4f70",
+    "url": "/static/media/paypal.1106b9c5.png"
+  },
+  {
+    "revision": "1eb7fff2a469da3cc6c3311572d7696d",
+    "url": "/static/media/placeholder.1eb7fff2.jpg"
+  },
+  {
+    "revision": "081bf019322fc100b3b84f34d9bff302",
+    "url": "/static/media/product1.081bf019.jpg"
+  },
+  {
+    "revision": "a0e400118b382fc007111de984da7552",
+    "url": "/static/media/product3.a0e40011.jpg"
+  },
+  {
+    "revision": "c46d3e6a35353394de17b2c6832779d8",
+    "url": "/static/media/ravspa.c46d3e6a.png"
+  },
+  {
+    "revision": "49b54380ba092ba68ab160f3dd8d5773",
+    "url": "/static/media/register.49b54380.jpeg"
+  },
+  {
+    "revision": "99a07353045b7026d5371197b5ea8fba",
+    "url": "/static/media/sidebar-1.99a07353.jpg"
+  },
+  {
+    "revision": "6706795b4468ca0eb2d5ba0ffa4fcc39",
+    "url": "/static/media/sidebar-2.6706795b.jpg"
+  },
+  {
+    "revision": "6706795b4468ca0eb2d5ba0ffa4fcc39",
+    "url": "/static/media/sidebar-3.6706795b.jpg"
+  },
+  {
+    "revision": "6706795b4468ca0eb2d5ba0ffa4fcc39",
+    "url": "/static/media/sidebar-4.6706795b.jpg"
+  }
+]);
---
+++ static/js/xlsx.e273c650.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpinvoicemate=this.webpackJsonpinvoicemate||[]).push([[3],{2157:function(n,i){},2217:function(n,i){},2218:function(n,i){}}]);
+//# sourceMappingURL=xlsx.e273c650.chunk.js.map
